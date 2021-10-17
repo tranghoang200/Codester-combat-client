6f074b869359e67ffc6458b536c45a389a312b89
Viet <vietnguyen19@augustana.edu>
2021-10-17 01:55:31 -0500

    Adde character selection page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import * as ROUTES from './routes';
 import gamePage from './component/game';
+import SelectionScreen from './component/player-selection/SelectionScreen';
 
 function App() {
   return (
     <Router>
       <Route path={ROUTES.GAME} component={gamePage} />
+      <Route path={ROUTES.PLAYER_SELECTION} component={SelectionScreen} />
     </Router>
   );
 }
