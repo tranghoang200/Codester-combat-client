8725b648e70608b55e591b5cce88a97f8784ad57
Trang Hoang <tranghoang200@gmail.com>
2021-10-17 18:57:15 -0500

    continue with board

--- src/Constant/routes.js
+++ src/Constant/routes.js
@@ -2,5 +2,5 @@ export const START = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const GAME = '/game';
-export const PLAYER_SELECTION = '/player';
-export const MATCHING_SCREEN = '/matching/:matchID';
+export const PLAYER_SELECTION = '/player/:matchID';
+export const MATCHING_SCREEN = '/matching';
